c64fa4a0ac1dcdb7fcde06107bfca6957fedcdee
pallavkhatri99 <pallavkhatri99@gmail.com>
2023-01-21 15:43:15 +0530

    Add Functionality or Details

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+1 for Home Page #home
+2 for About Page #about
+3 for Contact Page #contact
+*/
 var showContaint = 1
 function openCloseMenu(e){
     if(e == "open"){
@@ -5,12 +10,65 @@ function openCloseMenu(e){
         $('.nav span:contains("close")').css('display','block')
         $('.nav-item').removeClass('hide');
         $('#home').addClass('hide');
-        $('#about').addClass('hide')
+        $('#about').addClass('hide');
+        $('#contact').addClass('hide');
     }
-    else if(e == 'close'){
+    else if(e == "close"){
         $('.nav span:contains("menu")').css('display','block')
         $('.nav span:contains("close")').css('display','none')
         $('.nav-item').addClass('hide');
-        $('#home').removeClass('hide');
+        showPages()
     }
 }
+function show(show){
+    showContaint = show;
+    openCloseMenu("close");
+    showPages()
+}
+function showPages(e){
+    if(e != undefined){
+        showContaint = e
+    }
+    if(showContaint == 1){
+        $('#home').removeClass('hide');
+        $('#about').addClass('hide');
+        $('#contact').addClass('hide');
+    }
+    else if(showContaint == 2){
+        $('#about').removeClass('hide');
+        $('#home').addClass('hide');
+        $('#contact').addClass('hide');
+    }
+    else if(showContaint == 3){
+        $('#contact').removeClass('hide');
+        $('#home').addClass('hide');
+        $('#about').addClass('hide');
+    }
+}
+
+function copyEmail(e){
+    if(!$(e).find('.fa-clone').hasClass('hide')){
+        $(e).find('.fa-clone').addClass('hide');
+        navigator.clipboard.writeText(8740092045);
+        $(e).find('.fa-check').removeClass('hide');
+        alert("Copy");
+    }
+}
+function copyNumber(e){
+    if(!$(e).find('.fa-clone').hasClass('hide')){
+        $(e).find('.fa-clone').addClass('hide');
+        navigator.clipboard.writeText('pallavkhatri99@gmail.com');
+        $(e).find('.fa-check').removeClass('hide');
+        alert("Copy");
+    }
+}
+
+$('.btn-tabs button:last').hover(function(){
+    $('#education').removeClass("hide");
+    $('#experience').addClass("hide");
+})
+$('.btn-tabs button:first').hover(function(){
+    $('#experience').removeClass("hide");
+    $('#education').addClass("hide");
+})
+
